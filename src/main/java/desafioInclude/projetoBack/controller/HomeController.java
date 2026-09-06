c228c41e4ed3e8bedde676e540737557f053c5cd
Arthur Nogueira <arthurnogueiraza677@gmail.com>
2026-09-06 09:59:02 -0300

    Mudança nas permissões

---
+++ src/main/java/desafioInclude/projetoBack/controller/HomeController.java
@@ -0,0 +1,15 @@
+package desafioInclude.projetoBack.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Hidden
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/docs";
+    }
+}
